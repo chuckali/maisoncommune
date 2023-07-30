4058e54b0da3bf783dae89f07e5f530621a2de1b
chuckali <charlescusson@gmail.com>
2023-07-30 18:19:18 -0400

    5 html pages

---
+++ script.js
@@ -0,0 +1,27 @@
+
+//script.js
+// Function to copy text to clipboard using Clipboard API
+function copyToClipboard(text) {
+    navigator.clipboard.writeText(text).then(() => {
+      // Provide visual feedback to the user by adding the "clicked" class
+      const element = document.activeElement;
+      element.classList.add('clicked');
+      setTimeout(() => {
+        element.classList.remove('clicked'); // Remove the "clicked" class after a short delay
+      }, 1000);
+    }).catch((error) => {
+      console.error('Failed to copy text:', error);
+    });
+  }
+  
+  // Get all elements with the class "copy-text"
+  const copyTextElements = document.querySelectorAll('.copy-text');
+  
+  // Add event listeners to each element
+  copyTextElements.forEach((element) => {
+    element.addEventListener('click', () => {
+      // Get the text to copy from the data-copy attribute
+      const textToCopy = element.getAttribute('data-copy');
+      copyToClipboard(textToCopy);
+    });
+  });
